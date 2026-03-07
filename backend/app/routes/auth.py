1769e9abfa5f755e96d16bba7df57a6a5c37b0b8
fcorcar <fcorcar2006@g.educaand.es>
2026-03-07 23:02:39 +0100

    feat: Añadida validacion del formulario en el backend

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
 from app.models import db, Usuario
+from app.utils.validators import is_valid_email, is_valid_password
 
 # Creamos el Blueprint
 auth_bp = Blueprint('auth', __name__)
@@ -18,6 +19,14 @@ def registro():
     # Validar que nos envia todo
     if not email or not password or not name:
         return jsonify({"error": "Faltan datos"}), 400
+    
+    # Validar el email
+    if not is_valid_email(email):
+        return jsonify({"error": "El formato del correo electrónico no es válido"}), 400
+
+    # Validar contraseña  
+    if not is_valid_password(password):
+        return jsonify({"error": "La contraseña debe tener mínimo 6 caracteres, una mayúscula, una minúscula y un número"}), 400
 
     # 2. Comprobar si el usuario ya existe en la bd
     usuario_existente = Usuario.query.filter_by(email=email).first()
@@ -50,6 +59,10 @@ def login():
     datos = request.get_json()
     email = datos.get('email')
     password = datos.get('password')
+
+    # Valida el email y la contraseña
+    if not is_valid_email(email) or not is_valid_password(password):
+        return jsonify({"error": "Email o contraseña incorrectos"}), 401
 
     # 2. Comprobar si el usuario existe y si la contraseña coincide con el hash
     usuario = Usuario.query.filter_by(email=email).first()
